a42dcf3323b449f1c1f2903e062257de27bfb2db
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-09-27 11:05:02 +0800

    lmao

---
+++ CSDC23SaveToDB/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace CSDC23SaveToDB
+{
+    partial class studentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudID = new System.Windows.Forms.TextBox();
+            this.txtFF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.dtpBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // txtStudID
+            // 
+            this.txtStudID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudID.Location = new System.Drawing.Point(110, 64);
+            this.txtStudID.Name = "txtStudID";
+            this.txtStudID.Size = new System.Drawing.Size(100, 20);
+            this.txtStudID.TabIndex = 1;
+            // 
+            // txtFF
+            // 
+            this.txtFF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFF.Location = new System.Drawing.Point(110, 90);
+            this.txtFF.Name = "txtFF";
+            this.txtFF.Size = new System.Drawing.Size(233, 20);
+            this.txtFF.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name";
+            // 
+            // txtLN
+            // 
+            this.txtLN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLN.Location = new System.Drawing.Point(110, 116);
+            this.txtLN.Name = "txtLN";
+            this.txtLN.Size = new System.Drawing.Size(233, 20);
+            this.txtLN.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // txtMN
+            // 
+            this.txtMN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMN.Location = new System.Drawing.Point(110, 142);
+            this.txtMN.Name = "txtMN";
+            this.txtMN.Size = new System.Drawing.Size(233, 20);
+            this.txtMN.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Middle Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Birthdate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(53, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Gender";
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Location = new System.Drawing.Point(359, 67);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.Size = new System.Drawing.Size(455, 261);
+            this.dgvStudent.TabIndex = 12;
+            this.dgvStudent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudent_CellClick);
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cmbGender.Location = new System.Drawing.Point(110, 167);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(233, 21);
+            this.cmbGender.TabIndex = 13;
+            // 
+            // dtpBirthdate
+            // 
+            this.dtpBirthdate.Location = new System.Drawing.Point(110, 191);
+            this.dtpBirthdate.Name = "dtpBirthdate";
+            this.dtpBirthdate.Size = new System.Drawing.Size(233, 20);
+            this.dtpBirthdate.TabIndex = 14;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(12, 291);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 37);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(93, 291);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 37);
+            this.btnEdit.TabIndex = 16;
+            this.btnEdit.Text = "&Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(174, 291);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 37);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(268, 291);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 37);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "&Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // studentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpBirthdate);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.dgvStudent);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFF);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStudID);
+            this.Controls.Add(this.label1);
+            this.Name = "studentForm";
+            this.Text = "Student Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudID;
+        private System.Windows.Forms.TextBox txtFF;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvStudent;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.DateTimePicker dtpBirthdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CSDC23SaveToDB/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace CSDC23SaveToDB
+{
+    public partial class studentForm : Form
+    {
+        public studentForm()
+        {
+            InitializeComponent();
+        }
+
+        SQLiteConnection sql_con;
+        SQLiteCommand sql_cmd;
+        SQLiteDataAdapter DB;
+        DataSet DS = new DataSet();
+        DataTable DT =  new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void setConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source = StudentProfileDB.db");
+        }
+
+        private void loadData()
+        {
+            setConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from Student";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            dgvStudent.DataSource = DT;
+            sql_con.Close();
+        }
+
+        private void executeQuery(string txtQuery)
+        {
+            setConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "insert into Student (studentID,firstName, lastName, mdName, gender, birthDate) values('"+txtStudID.Text+"','"+txtFF.Text+ "','" + txtLN.Text + "','" + txtMN.Text + "','" + cmbGender.Text + "','" + dtpBirthdate.Value.ToShortDateString() + "')";
+
+            executeQuery(txtQuery);
+            loadData();
+
+            MessageBox.Show("Student profile has been successfully stored");
+            txtStudID.Clear();
+            txtFF.Clear();
+            txtLN.Clear();
+            txtMN.Clear();
+            dtpBirthdate.Value = DateTime.Now;
+        }
+
+        private void dgvStudent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtStudID.Text = dgvStudent.SelectedRows[0].Cells[1].Value.ToString();
+            txtFF.Text = dgvStudent.SelectedRows[0].Cells[2].Value.ToString();
+            txtLN.Text = dgvStudent.SelectedRows[0].Cells[3].Value.ToString();
+            txtMN.Text = dgvStudent.SelectedRows[0].Cells[4].Value.ToString();
+            cmbGender.Text = dgvStudent.SelectedRows[0].Cells[5].Value.ToString();
+            dtpBirthdate.Text = dgvStudent.SelectedRows[0].Cells[6].Value.ToString();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "update student set firstName = '" + txtFF.Text + "',lastName = '" + txtLN.Text + "',mdName= '" + txtMN.Text + "',gender = '" + cmbGender.Text + "',birthDate = '" + dtpBirthdate.Value.ToShortDateString() + "' where studentID = '"+txtStudID.Text+"'";
+
+            executeQuery(txtQuery);
+            loadData();
+
+            MessageBox.Show("Student profile has been successfully updated");
+            txtStudID.Clear();
+            txtFF.Clear();
+            txtLN.Clear();
+            txtMN.Clear();
+            dtpBirthdate.Value = DateTime.Now;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtStudID.Clear();
+            txtFF.Clear();
+            txtLN.Clear();
+            txtMN.Clear();
+            dtpBirthdate.Value = DateTime.Now;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "delete from student where studentID = '" + txtStudID.Text + "'";
+            executeQuery(txtQuery);
+          
+            loadData();
+
+            MessageBox.Show("Student profile has been successfully deleted");
+            txtStudID.Clear();
+            txtFF.Clear();
+            txtLN.Clear();
+            txtMN.Clear();
+            dtpBirthdate.Value = DateTime.Now;
+        }
+    }
+}
